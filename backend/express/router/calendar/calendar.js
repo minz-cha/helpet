843c74c01f9f0be34cb98730780248b32a1abd98
minju kim <demin1026@kpu.ac.kr>
2023-03-15 16:58:35 +0900

    [ADD] 캘린더 일정 수정 API 구현

--- backend/express/router/calendar/calendar.js
+++ backend/express/router/calendar/calendar.js
@@ -3,16 +3,17 @@ var router = express.Router();
 var db = require('../../db');
 
 // 달력화면
-router.get('/', function (request, response) {
-    var title = '달력화면';
-    response.json({ "title": title })
-});
+// router.get('/', function (request, response) {
+//     var title = '달력화면';
+//     response.json({ "title": title })
+// });
 
 // 달력일정 등록
 router.post('/add', (req, res) => {
     var date = req.body.date;
     var content = req.body.content;
     var userId = req.body.userId;
+    var errorcode = true;
 
     db.query('INSERT INTO calendar (cal_idx, userId, date, content) VALUES(?,?,?,?)', [null, userId, date, content], function (error, data) {
         if (error) throw error;
@@ -27,6 +28,7 @@ router.post('/add', (req, res) => {
 // 달력일정 삭제
 router.post('/delete', (req, res) => {
     var cal_idx = req.body.cal_idx;
+    var errorcode = true;
 
     db.query('DELETE FROM calendar where cal_idx = ?', [cal_idx], function (error, data) {
         if (error) throw error;
@@ -38,5 +40,22 @@ router.post('/delete', (req, res) => {
         })
     });
 })
+
+//달력일정 수정
+//이미 등록된 일정을 클릭 시
+router.post('/', (req, res) => {
+    var cal_idx = req.body.cal_idx;
+    var content = req.body.content;
+    var errorcode = true;
+
+    db.query('UPDATE calendar SET content = ? where cal_idx = ?', [content, cal_idx], function (err, data) {
+        if (error) throw err;
+        errorcode = false;
+        res.json({
+            "error": errorcode,
+            "modified content": content
+        })
+    })
+});
 
 module.exports = router;
