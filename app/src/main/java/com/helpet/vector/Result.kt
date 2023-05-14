7afd0d093737dd4f1820d87436964af6991abd27
기은서 <ellen1987@naver.com>
2023-05-14 21:01:23 +0900

    등록되어있는 반려동물 리스트, 반려동물 추가 등록 DB 연결

--- app/src/main/java/com/helpet/vector/Result.kt
+++ app/src/main/java/com/helpet/vector/Result.kt
@@ -3,17 +3,21 @@ package com.helpet.vector
 
 import com.google.gson.annotations.SerializedName
 
-data class Pet(
+data class Result(
     @SerializedName("petAge")
     val petAge: Int,
     @SerializedName("petBirth")
     val petBirth: String,
-    @SerializedName("petSpecies")
-    val petSpecies: String,
     @SerializedName("petGender")
     val petGender: String,
+    @SerializedName("petIdx")
+    val petIdx: Int,
     @SerializedName("petImg")
     val petImg: String,
     @SerializedName("petName")
-    val petName: String
+    val petName: String,
+    @SerializedName("petSpecies")
+    val petSpecies: String,
+    @SerializedName("userId")
+    val userId: String
 )
--- app/src/main/java/com/helpet/vector/petListResponseDTO.kt
+++ app/src/main/java/com/helpet/vector/petListResponseDTO.kt
@@ -3,8 +3,9 @@ package com.helpet.vector
 
 import com.google.gson.annotations.SerializedName
 
-data class PetResponseDto(
+data class petListResponseDTO(
+    @SerializedName("result")
+    val result: List<Result>,
     @SerializedName("status")
     val status: String
-
 )
