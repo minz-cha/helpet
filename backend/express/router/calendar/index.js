d370e0ed0f5178ab65344c3e6734cfe1600d80da
minju kim <demin1026@kpu.ac.kr>
2023-06-03 21:34:19 +0900

    [MODIFY] 캘린더 구조 수정 및 calendar api swagger 추가

---
+++ backend/express/router/calendar/index.js
@@ -0,0 +1,131 @@
+const calendarController = require("./calendar")
+const calendarRouter = require("express").Router()
+
+/**
+ * @swagger
+ * paths:
+ *  /api/calendar:
+ *    post:
+ *      summary: "캘린더접속시 당일 날짜를 기준으로 월별 데이터 조회"
+ *      description: "post방식: userId값 전송"
+ *      tags: [Calendar]
+ *      responses:
+ *        "200":
+ *          description: 당일 날짜 기준 월별 데이터 조회
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    status:
+ *                      type: string
+ *                      example: "success"
+ *                    month:
+ *                      type: int
+ *                      example: 6
+ *                    userId:
+ *                      type: string
+ *                      example: "asdf"
+ *                    results:
+ *                      type: object
+ *                      example:
+ *                          [
+ *                               {
+ *                                   "cal_idx": 1,
+ *                                   "userId": "asdf",
+ *                                   "date": "2023.06.01",
+ *                                   "month": 6,
+ *                                   "title": "test",
+ *                                  "description": "test content"
+ *                               },
+ *                               {
+ *                                   "cal_idx": 2,
+ *                                   "userId": "asdf",
+ *                                   "date": "2023.06.02",
+ *                                   "month": 6,
+ *                                   "title": "test2",
+ *                                  "description": "test content2"
+ *                               },
+ *                           ]
+ */
+calendarRouter.post('/', calendarController.calendarMain)
+
+/**
+ * @swagger
+ * paths:
+ *  /api/calendar/add:
+ *    post:
+ *      summary: "일정 등록"
+ *      tags: [Calendar]
+ *      responses:
+ *        "200":
+ *          description: 일정 등록 성공
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    status:
+ *                      type: string
+ *                      example: "success"
+ *                    message:
+ *                      type: string
+ *                      example: "일정이 등록되었습니다."
+ */
+calendarRouter.post('/add', calendarController.calendarAdd)
+
+/**
+ * @swagger
+ * paths:
+ *  /api/calendar/update:
+ *    post:
+ *      summary: "일정 수정"
+ *      tags: [Calendar]
+ *      responses:
+ *        "200":
+ *          description: 일정 수정 성공
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    status:
+ *                      type: string
+ *                      example: "success"
+ *                    title:
+ *                      type: string
+ *                      example: "수정된 제목"
+ *                    description:
+ *                      type: string
+ *                      example: "수정된 내용"
+ */
+calendarRouter.put('/update', calendarController.calendarUpdate)
+
+/**
+ * @swagger
+ * paths:
+ *  /api/calendar/delete:
+ *    post:
+ *      summary: "일정 삭제"
+ *      tags: [Calendar]
+ *      responses:
+ *        "200":
+ *          description: 일정 삭제 성공
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                    status:
+ *                      type: string
+ *                      example: "success"
+ *                    cal_idx:
+ *                      type: int
+ *                      example: 3
+ *                    message:
+ *                      type: string
+ *                      example: "일정이 삭제되었습니다."
+ */
+calendarRouter.delete('/delete', calendarController.calendarDelete)
+
+module.exports = calendarRouter
--- backend/express/router/main/main.js
+++ backend/express/router/main/main.js
@@ -2,12 +2,15 @@ const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser');
 
+const router = require("express").Router()
+
 const FileStore = require('session-file-store')(session)
 const app = express()
 
 var authRouter = require('../lib_login/auth');
 var authCheck = require('../lib_login/authCheck.js');
-var calendarRouter = require('../calendar/calendar');
+// var calendarRouter = require('../calendar/index');
+var calendar = require("../calendar");
 var communityRouter = require('../community/community');
 var petRouter = require('../pet/mypet');
 
@@ -18,6 +21,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 const port = 3000
 
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
@@ -27,17 +31,29 @@ app.use(session({
     store: new FileStore(),
 }))
 
+// swagger 문서화
+const { swaggerUi, specs } = require("../swagger/swagger");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs))
+
 // 세션
 app.get('/api/*', function (req, res, next) {
     if (req.session.userId == undefined) res.status(500).send('Something broke!');
     else next();
 });
 
+/**
+ * @swagger
+ * tags:
+ *   name: auth
+ *   description: 유저 추가 수정 삭제 조회
+ */
+
 // 인증 라우터
 app.use('/api/auth', authRouter);
 
-// 캘린더 라우터
-app.use('/api/calendar', calendarRouter);
+// // 캘린더 라우터
+app.use('/api/calendar', calendar);
+// router.calendar('api/calendar', calendar)
 
 // 커뮤니티 라우터
 app.use('/api/community', communityRouter);
